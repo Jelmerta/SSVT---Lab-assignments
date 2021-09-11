69100aa50e4e8d802da6a24acf99ff3df89ad42f
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2021-09-11 12:06:20 +0200

    finished ex 6

--- jelmer/Exercise6.hs
+++ jelmer/Exercise6.hs
@@ -1,11 +1,16 @@
-getPrimes :: [Integer] -> [Integer]
-getPrimes [] = [] -- Base case is required as this will recursively call getPrimes until each number is either added as a prime, or removed from the list due to being divisible by smaller number
-getPrimes (x:xs) = x : getPrimes [i | i <- xs, (i `mod` x) /= 0]
+import Lab1
+import Test.QuickCheck
+
+genPositiveIntegers :: Gen Integer
+genPositiveIntegers = abs <$> (arbitrary :: Gen Integer) `suchThat` (>0)
 
 productsPlusOne :: [Integer] -> Integer
-productsPlusOne xs = foldl (*) xs + 1
+productsPlusOne xs = (foldl (*) 1 xs) + 1
 
--- Pseudocode-ish... not working yet I want to get the first elements of the product plus one where the result is not prime
--- I need a prime check function for this?
-testConjecture :: Integer
-testConjecture = head $ filter (productsPlusOne (getPrimes [2..]) /= prime)
+-- -- Pseudocode-ish... not working yet I want to get the first elements of the product plus one where the result is not prime
+-- -- I need a prime check function for this?
+testConjecture :: Integer -> Bool
+testConjecture n = prime(productsPlusOne (take (fromIntegral n) primes))
+
+-- quickCheckResult $ forAll genPositiveIntegers testConjecture
+-- Fails after testing with n = 6
