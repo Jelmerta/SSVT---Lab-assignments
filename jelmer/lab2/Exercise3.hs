5522abbd71a93edd0899688fc0de12eaf61826ef
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2021-09-17 15:13:57 +0200

    ex3 finished, i think

--- jelmer/lab2/Exercise3.hs
+++ jelmer/lab2/Exercise3.hs
@@ -1,69 +1,50 @@
 -- Testing properties strength
+-- In this exercise we will compare the strength of different properties.
+-- To verify whether a property q is stronger than property p on some domain S,
+-- we have to check for every element e in domain S that if q(e) holds, then p(e) should also hold.
+-- If it is also the case that when p(e) holds then q(e) holds, then the two properties are equivalent.
 
-import Lab1
-
--- Provided functions for test properties
-stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
-stronger xs p q = forall xs (\ x -> p x --> q x)
-weaker   xs p q = stronger xs q p
+import Lab2
 
 -- a) Implement all properties from the Exercise 3 from Workshop 2
 --    as Haskell functions of type Int -> Bool.
 --    Consider a small domain like [(−10)..10].
 
+-- Property 1
 evenAndGreaterThan3 :: Integer -> Bool
-evenAndGreaterThan3 = \ x -> even x && x > 3 -- even n && (n > 3) -- \ x -> even x && x > 3
+evenAndGreaterThan3 n = even n && (n > 3)
 
+-- Property 2
 -- The even property is part of the Prelude library. It could simply be implemented as x `mod` 2 == 0
 
+-- Property 3
 evenOrGreaterThan3 :: Integer -> Bool
 evenOrGreaterThan3 n = even n || (n > 3)
 
--- This has the same effect as simply checking even, but just to be very explicit:
+-- Property 4: This has the same effect as simply checking even, but just to be very explicit:
 evenAndGreaterThan3OrEven :: Integer -> Bool
 evenAndGreaterThan3OrEven n = evenAndGreaterThan3 n || even n
 
-
--- stronger [-10..10] even evenAndGreaterThan3
--- stronger [-10..10] evenOrGreaterThan3 evenAndGreaterThan3
-
--- findStrengthOrder :: [a] [a -> Bool] -> [a -> Bool]
--- findStrengthOrder xs (p:props) = stronger xs p q : findStrengthOfindSt
-
-
--- stronger [10..10] evenOrGreaterThan3 even
-
--- To compare properties, we can make use of the following code, which will tell us if a property is stronger, weaker or equivalent
-compar :: [a] -> (a -> Bool) -> (a -> Bool) -> String
-compar xs p q = let pq = stronger xs p q
-                    qp = stronger xs q p
-                in
-                  if pq && qp then "equivalent"
-                  else if pq  then "stronger"
-                  else if qp  then "weaker"
-                  else             "incomparable"
-
-
-
-
--- Testing and printing
 exercise3 :: IO ()
 exercise3 = do
-    putStrLn "\n--- Exercise 3 ---\n"
-    putStrLn "W2EX3: Comparing the function evenAndGreaterThan3 (\\ x -> even x && x > 3) with the function even gives using the function stronger in the range of [-10..10] gives us:"
-    compar [0..10] even (even Lecture2..&&. (>3))
-    compar [0..10] even (even Lecture2..||. (>3))
+    -- b) Provide a descending strength list of all the implemented properties.
+    putStrLn "\n--- Exercise 3 ---\n\n"
+    putStrLn "W2EX3.1: Comparison of the function evenAndGreaterThan3 (even n && (n > 3)) with the function even in the range of [-10..10] tells us that evenAndGreaterThan3 is:"
+    print (compar [-10..10] evenAndGreaterThan3 even)
+    putStrLn "We now know that order of strength for the properties is [evenAndGreaterThan3, even]"
+    putStrLn ""
 
-    print (stronger [-10..10] evenAndGreaterThan3 even)
-    putStrLn "which means that the order of property strength is now:"
-    putStrLn "[evenAndGreaterThan3, even]"
+    putStrLn "W2EX3.2: Comparison of the function evenOrGreaterOr3 (even n || (n > 3)) with the function even in the range of [-10..10] tells us that evenOrGreaterThan3 is:"
+    print (compar [-10..10] evenOrGreaterThan3 even)
+    putStrLn "We now know that order of strength for the properties is [evenAndGreaterThan3, even, evenOrGreaterOr3]"
+    putStrLn ""
 
-    putStrLn "Next, we can compare"
+    putStrLn "W2EX3.3: Comparison of the function evenAndGreaterThan3OrEven (evenAndGreaterThan3 n || even n) with the function even in the range of [-10..10] tells us that evenAndGreaterThan3OrEven is:"
+    print (compar [-10..10] evenAndGreaterThan3OrEven even)
+    putStrLn "We now know that order of strength for the properties is [evenAndGreaterThan3, even, evenAndGreaterThan3OrEven, evenOrGreaterOr3], evenAndGreaterThan3OrEven and even being equivalent"
+    putStrLn ""
 
-    -- (\ x -> even x && x > 3) or even
-    -- \ = n(n+1)(2*n+1)/6 for natural numbers n:"
-    -- quickCheckResult $ forAll genPositiveIntegers testSumSquares
-    -- putStrLn "\nW1EX3: Using Quickcheck to test the property 1^3 + 2^3 + ... + n^3\
-    -- \ = (n(n+1)/2)^2 for natural numbers n:"
-    -- quickCheckResult $ forAll genPositiveIntegers testSumCubes
-    -- putStrLn "\n"
+    putStrLn "W2EX3.4: Comparison of the function even with the function evenAndGreaterThan3OrEven (evenAndGreaterThan3 n || even n) in the range of [-10..10] tells us that even is:"
+    print (compar [-10..10] even evenAndGreaterThan3OrEven)
+    putStrLn "We now know that order of strength for the properties is [evenAndGreaterThan3, even, evenAndGreaterThan3OrEven, evenOrGreaterOr3], evenAndGreaterThan3OrEven and even being equivalent"
+    putStrLn ""
