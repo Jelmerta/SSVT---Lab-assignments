0b68669e384341417e221b37fc7726014709ce5b
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2021-09-22 15:09:50 +0200

    added empty exercises for lab 3

---
+++ jelmer/lab3/Exercise1.hs
@@ -0,0 +1,5 @@
+module Exercise1 where
+
+exercise1 :: IO ()
+exercise1 = do
+    putStrLn "\n--- Exercise 1 ---\n\n"
---
+++ jelmer/lab3/Exercise6.hs
@@ -0,0 +1,5 @@
+module Exercise6 where
+
+exercise1 :: IO ()
+exercise1 = do
+    putStrLn "\n--- Exercise 6 ---\n\n"
