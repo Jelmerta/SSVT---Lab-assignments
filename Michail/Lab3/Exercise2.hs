5481ae43500399ce2ddd63b9d3a1e3480b7e1275
Michail Skalistiris Pikis <michaelskalistirispikis@gmail.com>
2021-09-23 17:11:52 +0200

    Lab3 ex1, ex2

---
+++ Michail/Lab3/Exercise2.hs
@@ -0,0 +1,16 @@
+module Exercise2 where
+
+import Lecture3
+import SetOrd 
+import Data.List
+import System.Random
+import Test.QuickCheck
+
+-- The lecture notes of this week define a function parse for parsing propositional formulas. 
+-- Test this function. You can use any test method you want.
+
+-- parse :: String -> [Form]
+-- Is a functions that takes as input a string representing a propositionbal formula and translates it into Form data type
+
+-- Preconditions:
+---- 
