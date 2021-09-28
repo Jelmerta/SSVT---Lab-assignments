ca12eb88b195a32d5af7d82fcc7344ed353805ab
nickmoone <nick.moone98@gmail.com>
2021-09-28 21:42:29 +0200

    Changed main function calls.

--- submission/Lab3/Main.hs
+++ submission/Lab3/Main.hs
@@ -1,7 +1,6 @@
 import Exercise1
 import Exercise2
-import Exercise3
-import Exercise4
+import Exercise3And4
 import Exercise5
 import Exercise6
 import Bonus1
@@ -12,8 +11,7 @@ main :: IO ()
 main = do
     exercise1
     exercise2
-    exercise3
-    exercise4
+    exercise3And4
     exercise5
     exercise6
     putStrLn "\n--- Bonus assignments: Project Euler ---\n"
