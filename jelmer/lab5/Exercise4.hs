ad92475f8f4bf584a0e3d2b36da0c7300f4816a8
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2021-10-12 21:10:42 +0200

    think after works correctly now?

--- jelmer/lab5/Exercise4.hs
+++ jelmer/lab5/Exercise4.hs
@@ -3,21 +3,39 @@ import LTS
 import Data.List
 import Test.QuickCheck
 import Exercise3
+import Data.Maybe
 
-removeBeforeAll :: [Trace] -> Trace -> [Trace]
-removeBeforeAll traces trace = map (`removeBefore` trace) traces
+-- Find list of possible states after performing the current label on the current state
+states :: [LabeledTransition] -> Label -> State -> [State]
+states transitions label state = catMaybes([states' transition label state | transition <- transitions])
 
-removeBefore :: Trace -> Trace -> Trace
-removeBefore [] [] = []
-removeBefore [] tracePartial = []
-removeBefore trace [] = trace
-removeBefore (headFull:traceFull) (headPartial:tracePartial) 
-    | headFull == headPartial = removeBefore traceFull tracePartial
-    | otherwise = headFull:traceFull
+states' :: LabeledTransition -> Label -> State -> Maybe State
+states' (s0, l, s1) label state
+    | l == label && s0 == state = Just s1
+    | otherwise = Nothing
 
--- TODO Will return endless results in some cases
-after :: IOLTS -> Trace -> [Trace]
-after iolts = removeBeforeAll (straces iolts)
+after' :: [LabeledTransition] -> Trace -> State -> [State]
+after' _ [] state = [state]
+after' transitions (t1:trace) state = concatMap (after' transitions trace) (states transitions t1 state)
+
+infix 1 `after`
+after :: IOLTS -> Trace -> [State]
+after (_,_,_,t,q0) trace = after' t trace q0
+
+
+-- Initial implementation... Not correct
+-- removeBeforeAll :: [Trace] -> Trace -> [Trace]
+-- removeBeforeAll traces trace = map (`removeBefore` trace) traces
+
+-- removeBefore :: Trace -> Trace -> Trace
+-- removeBefore [] [] = []
+-- removeBefore [] tracePartial = []
+-- removeBefore trace [] = trace
+-- removeBefore (headFull:traceFull) (headPartial:tracePartial) 
+--     | headFull == headPartial = removeBefore traceFull tracePartial
+--     | otherwise = headFull:traceFull
+
+
 
 exercise4 :: IO ()
 exercise4 = do
