789efdeca8626d0cbb6fe5caeb2bb41d7bf7dacb
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2021-10-14 11:57:45 +0200

    finish ex1

--- submission/Lab6/mutation_testing/stryker-js/packages/instrumenter/src/mutators/uva-mutator.ts
+++ submission/Lab6/mutation_testing/stryker-js/packages/instrumenter/src/mutators/uva-mutator.ts
@@ -1,9 +1,26 @@
+import * as types from '@babel/types';
+
 import { NodeMutator } from '.';
+
+enum UpdateOperators {
+  '+=' = '-=',
+  '-=' = '+=',
+}
 
 export const uvaMutator: NodeMutator = {
   name: 'UvaMutator',
 
   *mutate(path) {
-    // TODO Implement
+    if (path.isAssignmentExpression() && isSupported(path.node.operator)) {
+      const mutatedOperator = UpdateOperators[path.node.operator];
+
+      const replacement = types.cloneNode(path.node, true);
+      replacement.operator = mutatedOperator;
+      yield replacement;
+    }
   },
 };
+
+function isSupported(operator: string): operator is UpdateOperators {
+  return Object.keys(UpdateOperators).includes(operator);
+}
